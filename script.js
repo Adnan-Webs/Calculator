6366d6e5380570c8a5c8af8e3ce3059f80f1bfc7
unknown <adnanhusseinsocial@gmail.com>
2025-06-22 21:44:01 +0300

    Update calculator UI and theme logic

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-// Appends the specified character to the display
 function append(x) {
   document.getElementById("display").value += x;
 }
 
-// Clears the display by setting its value to an empty string
 function clearDisplay() {
   document.getElementById("display").value = "";
 }
 
-// Calculates the result of the mathematical expression in the display
 function calculate() {
   try {
-    var result = document.getElementById("display").value; // Get the current expression
-    result = eval(result); // Evaluate the expression
-    document.getElementById("display").value = result; // Update the display with the result
+    var result = document.getElementById("display").value;
+
+    result = eval(result);
+
+    document.getElementById("display").value = result;
   } catch (e) {
-    document.getElementById("display").value = "error"; // Show "error" if evaluation fails
+    document.getElementById("display").value = "error";
   }
 }
 
-// Removes the last character from the display
 function backspace() {
-  var display = document.getElementById("display").value; // Get the current value of the display
-  document.getElementById("display").value = display.slice(0, -1); // Remove the last character
+  var display = document.getElementById("display").value;
+  document.getElementById("display").value = display.slice(0, -1);
+}
+
+function lightMode() {
+  const body = document.body;
+  const bgColor = window.getComputedStyle(body).backgroundColor;
+  if (bgColor === "rgb(255, 255, 255)") {
+    return;
+  }
+  body.style.backgroundColor = "white";
+}
+
+function darkMode() {
+  const body = document.body;
+  const bgColor = window.getComputedStyle(body).backgroundColor;
+  if (bgColor === "rgb(0, 0, 0)") {
+    return;
+  }
+  body.style.backgroundColor = "black";
 }
